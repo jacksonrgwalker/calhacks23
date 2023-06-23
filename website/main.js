5066df42ed651f4408e7420152b9f5ae754e6c91
Jackson Walker <jacksonrgwalker@gmail.com>
2023-06-23 15:50:50 -0700

    more refactoring

--- website/main.js
+++ website/main.js
@@ -1,6 +1,3 @@
-// Contant variables
-var saved_categories = {};
-
 // Main parameters of the module
 const CATEGORIES = {
   "History": [
@@ -45,7 +42,6 @@ const CATEGORIES = {
   ]
 };
 
-
 // Additional Style categories
 const Style_CATEGORIES = {
   "Style": [
@@ -89,7 +85,6 @@ const Style_CATEGORIES = {
   ],
 };
 
-
 // Helper function to replace vars into a string
 String.prototype.format = function () {
   var args = arguments;
@@ -97,6 +92,13 @@ String.prototype.format = function () {
     return typeof args[index] !== 'undefined' ? args[index] : match;
   });
 };
+
+// Function to convert a string to title case
+function toTitleCase(str) {
+  return str.replace(/\w\S*/g, function (txt) {
+    return txt.charAt(0).toUpperCase() + txt.substr(1).toLowerCase();
+  });
+}
 
 // Helper function to generate a modal 
 function createModal(title, content) {
@@ -124,15 +126,38 @@ function createModal(title, content) {
   modal.appendTo($(document));
 }
 
+// Function to scroll to the last element of the chat
+function scrollLast() {
+  const target = document.getElementById("main-chat");
+  const scrollHeight = target.scrollHeight;
+  target.scrollTop = scrollHeight;
+}
+
+// Sets up the event listeners that will be used globally
+function addAllEventListeners() {
+
+  // Event listener for the clicking the "Submit message" button
+  document.querySelector("#chat-submission-button").addEventListener("click", submitUserMessage);
+
+  // Event listener for pressing Enter key in the input field
+  document.querySelector("#chat-input").addEventListener("keypress", function (e) {
+    // check that it was the enter key that was pressed
+    if (e.key === 'Enter') {
+      submitUserMessage(e);
+    }
+  }
+  );
+}
+
 // Function to add a user message to the chat
 function addUserMessage(message) {
 
   // Create the outer bubble and set bootstrap classes
-  let userMessageBubble = document.createElement("div");
+  const userMessageBubble = document.createElement("div");
   userMessageBubble.setAttribute("class", "w-100 d-flex justify-content-end");
 
   // Create the inner text and set bootstrap classes
-  let userMessageText = document.createElement("div");
+  const userMessageText = document.createElement("div");
   userMessageText.setAttribute("class", "w-75 alert alert-success");
   userMessageText.setAttribute("role", "alert");
   userMessageText.textContent = message;
@@ -149,11 +174,11 @@ function addUserMessage(message) {
 function addAIMessage(message) {
 
   // Create the outer bubble and set bootstrap classes
-  let aiMessageBubble = document.createElement("div");
+  const aiMessageBubble = document.createElement("div");
   aiMessageBubble.setAttribute("class", "w-100 d-flex justify-content-start");
 
   // Create the inner text and set bootstrap classes
-  let aiMessageText = document.createElement("div");
+  const aiMessageText = document.createElement("div");
   aiMessageText.setAttribute("class", "w-75 alert alert-info");
   aiMessageText.setAttribute("role", "alert");
   aiMessageText.textContent = message;
@@ -168,38 +193,42 @@ function addAIMessage(message) {
 
 // Function to add a message selection modal in bootstrap
 function addPreparedMessageSelector(options, callback) {
-  var options_elem = $('<div class="w-100 d-flex justify-content-start"><div class="w-75 list-group"></div></div>');
-  var options_elem_list = options_elem.find(".list-group");
 
-  for (var i = 0; i < options.length; i++) {
-    var button = $(`<button type="button" class="list-group-item list-group-item-action">${options[i]}</button>`);
-    options_elem_list.append(button);
+  const options_elem = document.createElement("div");
+  options_elem.classList.add("w-100", "d-flex", "justify-content-start");
+  const options_elem_list = document.createElement("div");
+  options_elem_list.classList.add("w-75", "list-group");
+  options_elem.appendChild(options_elem_list);
 
-    button.click(function (event) {
-      event.preventDefault();
-      var selectedOption = $(this).text();
-      options_elem.remove();
-      callback(selectedOption); // Trigger the callback with the selected option
-      // simulateAIResponse(selectedOption); // Simulate AI response after option selection
-    });
+  // callback for when a button is clicked
+  buttonClickCallback = function (event) {
+    event.preventDefault();
+    const selectedOption = this.textContent;
+    options_elem.remove();
+    callback(selectedOption);
   }
 
-  options_elem.appendTo($("#main-chat"));
+  // create the buttons for each option
+  for (var i = 0; i < options.length; i++) {
+
+    const button = document.createElement("button");
+    button.type = "button";
+    button.classList.add("list-group-item", "list-group-item-action");
+    button.textContent = options[i];
+    options_elem_list.appendChild(button);
+
+    button.addEventListener("click", buttonClickCallback);
+  }
+
+  document.getElementById("main-chat").appendChild(options_elem);
   scrollLast();
 }
 
-// Function to scroll to the last element of the chat
-function scrollLast() {
-  var target = $("#main-chat");
-  var scrollHeight = target.prop("scrollHeight");
-  target.scrollTop(scrollHeight);
-}
-
 // Function to handle the user message submission 
-function submitUserMessage(event) {
+function submitUserMessage(message) {
 
-  // Get the user message from the input field
-  var userMessage = document.querySelector("#chat-input").value;
+  // if `message` argument is an event, get the value from the input field
+  const userMessage = message instanceof Event ? document.getElementById("chat-input").value : message;
 
   // if the user message is empty, do nothing
   if (userMessage === "") {
@@ -213,183 +242,176 @@ function submitUserMessage(event) {
   sendUserMessageToAI(userMessage);
 
   // Clear the chat input field 
-  document.querySelector("#chat-input").value = "";
+  document.getElementById("chat-input").value = "";
 
 };
 
-function addAllEventListeners() {
+// Populates one column of categories in the modal
+function generateCategoryLine(category, items) {
 
-  // Event listener for the clicking the "Submit message" button
-  document.querySelector("#chat-submission-button").addEventListener("click", submitUserMessage);
+  const categoryLine = document.createElement("div");
+  categoryLine.classList.add("w-100", "d-flex", "justify-content-start");
 
-  // Event listener for pressing Enter key in the input field
-  document.querySelector("#chat-input").addEventListener("keypress", function (e) {
-    // check that it was the enter key that was pressed
-    if (e.key === 'Enter') {
-      submitUserMessage(e);
-    }
+  const categoryTitle = document.createElement("div");
+  categoryTitle.classList.add("w-25");
+  categoryTitle.textContent = toTitleCase(category) + ":";
+
+  categoryTitle.append(categoryLine);
+
+  const selectGroup = document.createElement("div");
+  selectGroup.classList.add("form-group", "w-75", "mb-2");
+
+  const selectElem = document.createElement("select");
+  selectElem.classList.add("form-control");
+  selectElem.setAttribute("id", category);
+
+  for (let i = 0; i < items.length; i++) {
+    const option = document.createElement("option");
+    option.setAttribute("value", items[i]);
+    option.textContent = items[i];
+    selectElem.appendChild(option);
   }
-  ); 
-  
+
+  const listParams = document.getElementById("list-parameters");
+
+  selectGroup.appendChild(selectElem);
+  categoryLine.appendChild(selectGroup);
+  listParams.appendChild(selectGroup);
 }
 
+// Populates the modal with globally defined categories and their options
+function populateCategoryDropdowns() {
+
+  Object.entries(CATEGORIES).forEach(([category, items]) => {
+    generateCategoryLine(category, items);
+  });
+
+  Object.entries(Style_CATEGORIES).forEach(([category, items]) => {
+    generateCategoryLine(category, items);
+  });
+
+}
+
+// commits the selected parameters, disables the dropdowns, and removes the confirm button
+function saveAndResetChosenParameters() {
+  const selects = listParams.querySelectorAll("#list-parameters select");
+  document.chosenParameters = [];
+
+  for (let i = 0; i < selects.length; i++) {
+    const selectId = selects[i].getAttribute("id");
+    const selectedOption = selects[i].value;
+
+    if (selectedOption === null) {
+      createModal(
+        "Error",
+        `Category "${selectId}" has no selected option.`
+      );
+      return;
+    }
+    document.chosenParameters.push(selectedOption);
+  }
+
+  // Disable all elements
+  for (let i = 0; i < selects.length; i++) {
+    selects[i].disabled = true;
+  }
+
+  // Remove the confirm button
+  document.getElementById("confirm-params-button").remove();
+}
+
+// send the initial character selection to the AI starting the game
+function sendCharacterSelectionToAI() {
+
+  const charSelection = {
+    background: document.chosenParameters[0],
+    trait: document.chosenParameters[1],
+    location: document.chosenParameters[2],
+    goal: document.chosenParameters[3],
+    item: document.chosenParameters[4],
+  };
+
+  // get game update obj from fastapi python server
+  let endpoint = "http://127.0.0.1:8000/game/start"
+
+  gameUpdatePromise = fetch(endpoint, {
+    method: 'POST',
+    mode: "cors", // no-cors, *cors, same-origin
+    cache: "no-cache", // *default, no-cache, reload, force-cache, only-if-cached
+    credentials: "same-origin", // include, *same-origin, omit
+    headers: {
+      'Accept': 'application/json',
+      'Content-Type': 'application/json',
+      'Access-Control-Allow-Origin': '*',
+      'Access-Control-Request-Methods': '*',
+      'Origin': 'http://jwalk.io'
+    },
+    body: JSON.stringify(charSelection)
+  });
+
+  document.gameUpdate = gameUpdatePromise.then(response => response.json());
+
+}
+
+// Local function to handle the category submission
+async function handleConfirmParameters(event) {
+
+  event.preventDefault();
+  saveAndResetChosenParameters();
+  sendCharacterSelectionToAI();
+  const gameUpdate = await document.gameUpdate;
+
+  addAIMessage(gameUpdate.player_message)
+
+  updateHealthBar(gameUpdate.player_stats[0]);
+  updateEnergyBar(gameUpdate.player_stats[1]);
+  updateMoneyBar(gameUpdate.player_stats[2]);
+
+  // Generate message selection modal
+  addPreparedMessageSelector(gameUpdate.action_options, submitUserMessage);
+}
 
 // Generate categories and handlers
 function generateCategoriesAndHandlers() {
-  var root_elem = $("#list-parameters");
 
-  // Generate all the inputs from the CATEGORIES constant
-  Object.entries(CATEGORIES).forEach(function ([category, items]) {
-    var categoryLine = $('<div class="w-100 d-flex justify-content-start"></div>');
-    var categoryTitle = $('<div class="w-25">' + toTitleCase(category) + ':</div>');
-    categoryTitle.appendTo(categoryLine);
+  populateCategoryDropdowns();
 
-    var selectGroup = $('<div class="form-group w-75 mb-2"></div>');
-    var selectElem = $('<select class="form-control"></select>');
-    selectElem.attr('id', category);
+  listParams = document.getElementById("list-parameters");
 
-    for (var i = 0; i < items.length; i++) {
-      var option = $('<option value="' + items[i] + '">' + items[i] + '</option>');
-      option.appendTo(selectElem);
-    }
+  const validationButton = document.createElement("form");
+  validationButton.classList.add("w-100", "form-inline", "d-flex", "justify-content-center");
+  validationButton.id = "confirm-params-button";
 
-    selectElem.appendTo(selectGroup);
-    selectGroup.appendTo(categoryLine);
-    categoryLine.appendTo(root_elem);
-  });
+  const button = document.createElement("button");
+  button.classList.add("btn", "btn-primary");
+  button.setAttribute("type", "submit");
+  button.textContent = "Confirm parameters";
 
-  // Generate all the inputs from the Style_CATEGORIES constant
-  Object.entries(Style_CATEGORIES).forEach(function ([category, items]) {
-    var categoryLine = $('<div class="w-100 d-flex justify-content-start"></div>');
-    var categoryTitle = $('<div class="w-25">' + toTitleCase(category) + ':</div>');
-    categoryTitle.appendTo(categoryLine);
+  validationButton.appendChild(button);
+  validationButton.addEventListener("submit", handleConfirmParameters);
+  listParams.append(validationButton);
 
-    var selectGroup = $('<div class="form-group w-75 mb-2"></div>');
-    var selectElem = $('<select class="form-control"></select>');
-    selectElem.attr('id', category);
-
-    for (var i = 0; i < items.length; i++) {
-      var option = $('<option value="' + items[i] + '">' + items[i] + '</option>');
-      option.appendTo(selectElem);
-    }
-
-    selectElem.appendTo(selectGroup);
-    selectGroup.appendTo(categoryLine);
-    categoryLine.appendTo(root_elem);
-  });
-
-  // Create the categories-wide validation button
-  var validation_button = $('<form class="w-100 form-inline d-flex justify-content-center">' +
-    '<button type="submit" class="btn btn-primary">Confirm parameters</button>' +
-    '</form>');
-  validation_button.submit(handleCategorySubmission);
-  validation_button.appendTo(root_elem);
-
-  // Local function to handle the category submission
-  async function handleCategorySubmission(event) {
-    event.preventDefault();
-
-    var selects = root_elem.find('select');
-
-    for (var i = 0; i < selects.length; i++) {
-      var select_id = $(selects[i]).attr("id");
-      var selectedOption = $(selects[i]).val();
-
-      if (selectedOption === null) {
-        createModal(
-          "Error",
-          'Category "' + select_id + '" has no selected option.'
-        );
-        return;
-      }
-
-      saved_categories[select_id] = selectedOption;
-    }
-
-    // Disable all elements
-    for (var i = 0; i < selects.length; i++) {
-      $(selects[i]).prop("disabled", true);
-    }
-
-    // Remove validation button
-    validation_button.remove();
-
-    // Generate AI message with selected parameters
-    var selectedParameters = [];
-    Object.entries(saved_categories).forEach(function ([category, value]) {
-      selectedParameters.push(value);
-    });
-
-    sysMsgPromptFmtr = {
-      background: selectedParameters[0],
-      trait: selectedParameters[1],
-      location: selectedParameters[2],
-      goal: selectedParameters[3],
-      item: selectedParameters[4],
-    };
-
-    // get game update obj from fastapi python server
-    let endpoint = "http://127.0.0.1:8000/game/start"
-
-    gameUpdatePromise = await fetch(endpoint, {
-      method: 'POST',
-      mode: "cors", // no-cors, *cors, same-origin
-      cache: "no-cache", // *default, no-cache, reload, force-cache, only-if-cached
-      credentials: "same-origin", // include, *same-origin, omit
-      headers: {
-        'Accept': 'application/json',
-        'Content-Type': 'application/json',
-        'Access-Control-Allow-Origin': '*',
-        'Access-Control-Request-Methods': '*',
-
-        'Origin': 'http://jwalk.io'
-      },
-      body: JSON.stringify(sysMsgPromptFmtr)
-    });
-
-    const gameUpdate = await gameUpdatePromise.json();
-
-    var aiMessage = gameUpdate.player_message
-    var aiOptions = gameUpdate.action_options
-
-    updateHealthBar(gameUpdate.player_stats[0]);
-    updateEnergyBar(gameUpdate.player_stats[1]);
-    updateMoneyBar(gameUpdate.player_stats[2]);
-
-    addAIMessage(aiMessage)
-    
-    preparedMessageCallback = function (aiOption) {
-      submitUserMessage(aiOption); // Submit the AI's selected option to the API
-      addUserMessage(aiOption); // Add the AI's selected option as a user message
-      $("#chat-submission-button").click(); // Trigger the button click to submit the AI's selected option
-    }
-
-    // Generate message selection modal
-    addPreparedMessageSelector(aiOptions, preparedMessageCallback);
-  }
-
-  function updateHealthBar(health) {
-    var healthBar = $("#healthbar");
-    healthBar.css("width", health + "%");
-  }
-
-  function updateEnergyBar(energy) {
-    var energyBar = $("#energybar");
-    energyBar.css("width", energy + "%");
-  }
-
-  function updateMoneyBar(money) {
-    var moneyBar = $("#moneybar");
-    moneyBar.css("width", money + "%");
-  }
-
-  // Function to convert a string to title case
-  function toTitleCase(str) {
-    return str.replace(/\w\S*/g, function (txt) {
-      return txt.charAt(0).toUpperCase() + txt.substr(1).toLowerCase();
-    });
-  }
 }
+
+// just updates the health bar on the page
+const updateHealthBar = (health) => {
+  const healthBar = document.querySelector("#healthbar");
+  healthBar.style.width = `${health}%`;
+};
+
+// just updates the energy bar on the page
+const updateEnergyBar = (energy) => {
+  const energyBar = document.querySelector("#energybar");
+  energyBar.style.width = `${energy}%`;
+};
+
+// just updates the money bar on the page
+const updateMoneyBar = (money) => {
+  const moneyBar = document.querySelector("#moneybar");
+  moneyBar.style.width = `${money}%`;
+};
+
+///////////////////////////////////////////
 
 addAllEventListeners();
 generateCategoriesAndHandlers();
